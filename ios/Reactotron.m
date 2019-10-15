135ba77fb0aaccead6bd49b10cbe4ad85fd53a39
Richard Evans <rmevans9@gmail.com>
2019-10-14 20:53:20 -0400

    fix: Remove compile directive

--- ios/Reactotron.m
+++ ios/Reactotron.m
@@ -1,5 +1,3 @@
-#if FB_SONARKIT_ENABLED
-
 #import "Reactotron.h"
 #import "ReactotronFlipperPlugin.h"
 
@@ -18,10 +16,8 @@ RCT_EXPORT_METHOD(sendCommand:(NSString *)method command:(NSDictionary *)obj)
 {
     FlipperClient *client = [FlipperClient sharedClient];
     ReactotronFlipperPlugin *myPlugin = [client pluginWithIdentifier:@"flipper-plugin-reactotron"];
-    
+
     [myPlugin sendCommand:method command:obj];
 }
 
 @end
-
-#endif
