dfc2095fa764374c0a5660334a50a6058eaffdb5
rmevans9 <rmevans9@gmail.com>
2019-11-02 22:50:58 -0400

    feat: Setup to allow for messages to flow back to the client

--- ios/ReactotronFlipperPlugin.h
+++ ios/ReactotronFlipperPlugin.h
@@ -3,10 +3,15 @@
 #import <FlipperKit/FlipperConnection.h>
 #import <FlipperKit/FlipperPlugin.h>
 
+typedef void (^commandFromDesktop)(NSDictionary*);
+
 @interface ReactotronFlipperPlugin : NSObject<FlipperPlugin>
 
 @property id<FlipperConnection> connection;
+@property commandFromDesktop commandFromDesktopReceivedHandler;
 
 -(void) sendCommand:(NSString*)seralizedCommand command:(NSDictionary *)obj;
+
+-(void) receiveCommand:(commandFromDesktop) compblock;
 
 @end
